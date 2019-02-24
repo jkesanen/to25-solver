138a5a9a94ff5d4f720c9a39aefe29d05c78993c
Jani Kesänen <jani.kesanen+github@gmail.com>
2019-02-24 16:37:26 +0200

    Added a design related note and logical ordering of methods.

--- src/board.hpp
+++ src/board.hpp
@@ -16,22 +16,23 @@ public:
     Board(const std::size_t width, const std::size_t height);
 
     bool insert(const position_t position);
-
     void pop_back();
 
     std::optional<position_t> latest() const;
-
-    void output() const;
-
     std::size_t width() const;
     std::size_t height() const;
-
     bool isSolved() const;
+
+    void output() const;
 
 private:
     std::size_t mWidth;
     std::size_t mHeight;
 
+    /** NOTE: Using std::set + std::vector for convenience:
+     *    - set for easy and efficient duplicate matching.
+     *    - vector for ordering.
+     */
     positions_t mPositions;
     order_t mOrder;
 };
